5b4b9a739b957b6ec6a97a65aa1845dbcb68d157
hajira0037 <hajira@med-examexpert.com>
2021-08-16 23:11:09 +0500

    file removed

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const path = require("path");
 const axios = require("axios");
 const app = express();
 
-const helmet = require('helmet') // creates headers that protect from attacks (security)
-const cors = require('cors')  // allows/disallows cross-site communication
-
+const helmet = require("helmet"); // creates headers that protect from attacks (security)
+const cors = require("cors"); // allows/disallows cross-site communication
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,41 +12,40 @@ app.use(express.json());
 
 // --> Add this
 // ** MIDDLEWARE ** //
-const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.herokuapp.com']
+const whitelist = ["http://localhost:3000", "http://localhost:8080"];
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
+    console.log("** Origin of request " + origin);
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
+      console.log("Origin acceptable");
+      callback(null, true);
     } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
-app.use(helmet())
+  },
+};
+app.use(helmet());
 // --> Add this
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.get('/api/', (req, res) => {
+app.get("/api/", (req, res) => {
   axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
     res.send(response.data);
   });
 });
 
-
 // --> Add this
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, (req, res) => {
-    console.log(`server listening on port: ${PORT}`)
-  });
+  console.log(`server listening on port: ${PORT}`);
+});
