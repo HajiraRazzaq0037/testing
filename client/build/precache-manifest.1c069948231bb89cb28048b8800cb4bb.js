e0737e5a5a6b7682147ae293ce2de00e8313e416
hajira <hajirarazzaq@gap-dynamics>
2021-08-17 13:25:50 +0500

    app extension changed

---
+++ client/build/precache-manifest.1c069948231bb89cb28048b8800cb4bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f696e8e440d26d6243a027c19d3530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab08a7a63c4c4955faef",
+    "url": "/static/css/main.cae252f0.chunk.css"
+  },
+  {
+    "revision": "8dd34b197d13be367dff",
+    "url": "/static/js/2.900ccd78.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.900ccd78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab08a7a63c4c4955faef",
+    "url": "/static/js/main.4d0caa68.chunk.js"
+  },
+  {
+    "revision": "54ab41ca9e345de0f752",
+    "url": "/static/js/runtime-main.36ba75b2.js"
+  }
+]);
