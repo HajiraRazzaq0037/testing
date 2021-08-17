f8622ad7ecac7a1c9af9412657d317717ba84341
hajira <hajirarazzaq@gap-dynamics>
2021-08-17 13:58:39 +0500

    refinement

--- client/src/pages/homePage/homePage.js
+++ client/src/pages/homePage/homePage.js
@@ -1,48 +1,48 @@
-import React, { useEffect, useContext, useState } from 'react'
-import { GlobalContext } from '../../context/GlobalState'
-import firebase from '../../firebase'
-import axios from 'axios'
-import Header from '../../atoms/header'
+import React, { useEffect, useContext, useState } from "react";
+import { GlobalContext } from "../../context/GlobalState";
+import firebase from "../../firebase";
+import axios from "axios";
+import Header from "../../atoms/header";
 export default (props: any) => {
-  let { history } = props
-  const { state }: any = useContext(GlobalContext)
-  const [data, setData] = useState([])
+  let { history } = props;
+  const state = useContext(GlobalContext);
+  const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get('/api').then(response => {
-      setData(response.data)
-    })
-  }, [])
+    axios.get("/api").then((response) => {
+      setData(response.data);
+    });
+  }, []);
 
   return (
-    <div className=''>
+    <div className="">
       <Header>
         <button
-          className='p-8 bg-green-500 hover:bg-gren-300 w-full py-2 text-white'
+          className="p-8 bg-green-500 hover:bg-gren-300 w-full py-2 text-white"
           onClick={() => {
             firebase
               .auth()
               .signOut()
               .then(() => {
-                state.loader = true
-                history.push('/login')
-              })
+                state.loader = true;
+                history.push("/login");
+              });
           }}
         >
           Logout
         </button>
       </Header>
 
-      <div className='py-5'>
-        <div className='mt-8'>
+      <div className="py-5">
+        <div className="mt-8">
           {data.map((sin: any, index: number) => (
-            <div key={index} className='flex justify-between m-4'>
-              <p key={index} className='text-base mb-2 flex  font-bold w-1/4'>
+            <div key={index} className="flex justify-between m-4">
+              <p key={index} className="text-base mb-2 flex  font-bold w-1/4">
                 {sin.id} {sin.title}
               </p>
               <button
-                className=' bg-green-400 hover:bg-green-500 w-1/3 py-2 text-white w-1/4'
+                className=" bg-green-400 hover:bg-green-500 w-1/3 py-2 text-white w-1/4"
                 onClick={() => {
-                  props.history.push(`/${sin.id}`)
+                  props.history.push(`/${sin.id}`);
                 }}
               >
                 View Details
@@ -52,5 +52,5 @@ export default (props: any) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
