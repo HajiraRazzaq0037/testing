20176697aa7f6cca4cce54746ae1fcfc69563ca7
hajira <hajirarazzaq@gap-dynamics>
2021-08-17 09:45:48 +0500

    build app

--- index.js
+++ index.js
@@ -1,51 +1,22 @@
 const express = require("express");
+const serveStatic = require("serve-static");
 const path = require("path");
 const axios = require("axios");
 const app = express();
 
-const helmet = require("helmet"); // creates headers that protect from attacks (security)
-const cors = require("cors"); // allows/disallows cross-site communication
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.json());
-
-// --> Add this
-// ** MIDDLEWARE ** //
-const whitelist = ["http://localhost:3000", "http://localhost:8080"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin);
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable");
-      callback(null, true);
-    } else {
-      console.log("Origin rejected");
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-app.use(helmet());
-// --> Add this
-app.use(cors(corsOptions));
-
+//here we are configuring dist to serve app files
+app.use("/", serveStatic(path.join(__dirname, "/client/build")));
 app.get("/api/", (req, res) => {
   axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
     res.send(response.data);
   });
 });
 
-// --> Add this
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, (req, res) => {
-  console.log(`server listening on port: ${PORT}`);
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
+
+const port = process.env.PORT || 8081;
+app.listen(port);
+console.log(`app is listening on port: ${port}`);
